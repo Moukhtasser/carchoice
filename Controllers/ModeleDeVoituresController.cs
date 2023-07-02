a1d32c9b6399a3f4ccf66aa554439c62647f4618
Moukhtasser <salma.mouk@gmail.com>
2023-07-02 11:59:30 +0100

    init commit

---
+++ Controllers/ModeleDeVoituresController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using testPFA.Data;
+using testPFA.Models;
+
+namespace testPFA.Controllers
+{
+    public class ModeleDeVoituresController : Controller
+    {
+        private readonly YourDbContext _context;
+
+        public ModeleDeVoituresController(YourDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ModeleDeVoitures
+        public async Task<IActionResult> Index()
+        {
+              return _context.ModelesDeVoiture != null ? 
+                          View(await _context.ModelesDeVoiture.ToListAsync()) :
+                          Problem("Entity set 'YourDbContext.ModelesDeVoiture'  is null.");
+        }
+
+        // GET: ModeleDeVoitures/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.ModelesDeVoiture == null)
+            {
+                return NotFound();
+            }
+
+            var modeleDeVoiture = await _context.ModelesDeVoiture
+                .FirstOrDefaultAsync(m => m.IdModele == id);
+            if (modeleDeVoiture == null)
+            {
+                return NotFound();
+            }
+
+            return View(modeleDeVoiture);
+        }
+
+        // GET: ModeleDeVoitures/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ModeleDeVoitures/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdModele,Marque,Modele,Annee")] ModeleDeVoiture modeleDeVoiture)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(modeleDeVoiture);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(modeleDeVoiture);
+        }
+
+        // GET: ModeleDeVoitures/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.ModelesDeVoiture == null)
+            {
+                return NotFound();
+            }
+
+            var modeleDeVoiture = await _context.ModelesDeVoiture.FindAsync(id);
+            if (modeleDeVoiture == null)
+            {
+                return NotFound();
+            }
+            return View(modeleDeVoiture);
+        }
+
+        // POST: ModeleDeVoitures/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdModele,Marque,Modele,Annee")] ModeleDeVoiture modeleDeVoiture)
+        {
+            if (id != modeleDeVoiture.IdModele)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(modeleDeVoiture);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ModeleDeVoitureExists(modeleDeVoiture.IdModele))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(modeleDeVoiture);
+        }
+
+        // GET: ModeleDeVoitures/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.ModelesDeVoiture == null)
+            {
+                return NotFound();
+            }
+
+            var modeleDeVoiture = await _context.ModelesDeVoiture
+                .FirstOrDefaultAsync(m => m.IdModele == id);
+            if (modeleDeVoiture == null)
+            {
+                return NotFound();
+            }
+
+            return View(modeleDeVoiture);
+        }
+
+        // POST: ModeleDeVoitures/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.ModelesDeVoiture == null)
+            {
+                return Problem("Entity set 'YourDbContext.ModelesDeVoiture'  is null.");
+            }
+            var modeleDeVoiture = await _context.ModelesDeVoiture.FindAsync(id);
+            if (modeleDeVoiture != null)
+            {
+                _context.ModelesDeVoiture.Remove(modeleDeVoiture);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ModeleDeVoitureExists(int id)
+        {
+          return (_context.ModelesDeVoiture?.Any(e => e.IdModele == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Migrations/20230602190832_salM.cs
@@ -0,0 +1,179 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace testPFA.Migrations
+{
+    public partial class salM : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Loueurs",
+                columns: table => new
+                {
+                    IdLoueur = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Adresse = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telephone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Loueurs", x => x.IdLoueur);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ModelesDeVoiture",
+                columns: table => new
+                {
+                    IdModele = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Marque = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Modele = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Annee = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ModelesDeVoiture", x => x.IdModele);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Saisons",
+                columns: table => new
+                {
+                    IdSaison = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Saisons", x => x.IdSaison);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    IdClient = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Adresse = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telephone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LoueurIdLoueur = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.IdClient);
+                    table.ForeignKey(
+                        name: "FK_Clients_Loueurs_LoueurIdLoueur",
+                        column: x => x.LoueurIdLoueur,
+                        principalTable: "Loueurs",
+                        principalColumn: "IdLoueur",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reservations",
+                columns: table => new
+                {
+                    IdReservation = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClientIdClient = table.Column<int>(type: "int", nullable: false),
+                    VoitureIdModele = table.Column<int>(type: "int", nullable: false),
+                    IdSaison = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservations", x => x.IdReservation);
+                    table.ForeignKey(
+                        name: "FK_Reservations_Clients_ClientIdClient",
+                        column: x => x.ClientIdClient,
+                        principalTable: "Clients",
+                        principalColumn: "IdClient",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Reservations_ModelesDeVoiture_VoitureIdModele",
+                        column: x => x.VoitureIdModele,
+                        principalTable: "ModelesDeVoiture",
+                        principalColumn: "IdModele",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Reservations_Saisons_IdSaison",
+                        column: x => x.IdSaison,
+                        principalTable: "Saisons",
+                        principalColumn: "IdSaison",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Extras",
+                columns: table => new
+                {
+                    IdExtra = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomExtra = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Prix = table.Column<double>(type: "float", nullable: false),
+                    ReservationIdReservation = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Extras", x => x.IdExtra);
+                    table.ForeignKey(
+                        name: "FK_Extras_Reservations_ReservationIdReservation",
+                        column: x => x.ReservationIdReservation,
+                        principalTable: "Reservations",
+                        principalColumn: "IdReservation",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clients_LoueurIdLoueur",
+                table: "Clients",
+                column: "LoueurIdLoueur");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Extras_ReservationIdReservation",
+                table: "Extras",
+                column: "ReservationIdReservation");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_ClientIdClient",
+                table: "Reservations",
+                column: "ClientIdClient");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_IdSaison",
+                table: "Reservations",
+                column: "IdSaison");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_VoitureIdModele",
+                table: "Reservations",
+                column: "VoitureIdModele");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Extras");
+
+            migrationBuilder.DropTable(
+                name: "Reservations");
+
+            migrationBuilder.DropTable(
+                name: "Clients");
+
+            migrationBuilder.DropTable(
+                name: "ModelesDeVoiture");
+
+            migrationBuilder.DropTable(
+                name: "Saisons");
+
+            migrationBuilder.DropTable(
+                name: "Loueurs");
+        }
+    }
+}
